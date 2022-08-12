1598a9ee8ea63b267e07f9eb281decdc905fe83a
Alysson Marley Dias <callistomain@gmail.com>
2022-08-11 23:01:28 -0300

    Complete bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,9 @@
-// Variables
+// Init
+let dish, drink, dessert, prices, total;
 const form = document.querySelector("form");
-
-// Sorting inputs
-let inputs = [];
-for (const element of form.elements) {
-    if (element.type !== "fieldset") inputs.push(element);
-}
-
-// Execute
-addInputListeners();
+const inputs = document.querySelectorAll("input");
+const items = document.querySelectorAll(".item");
+inputs.forEach(e => e.addEventListener("click", validateForm));
 
 // Functions 
 function validateForm() {
@@ -16,37 +11,43 @@ function validateForm() {
         const button = document.querySelector("button");
         button.textContent = "Fechar pedido";
         button.classList.replace("disabled", "enabled");
-        button.addEventListener("click", executeOrder);
-        removeInputListeners();
+        button.addEventListener("click", confirmOrder);
+        inputs.forEach(e => e.removeEventListener("click", validateForm));
     }
 }
 
+function confirmOrder() {
+    const names = document.querySelectorAll("input:checked + label h4");
+    const values = document.querySelectorAll("input:checked + label h5");
+
+    [dish, drink, dessert] = [...names].map(e => e.textContent);
+    prices = [...values].map(e => priceToNumber(e.textContent));
+    total = prices.reduce((a, b) => a + b, 0).toFixed(2);
+
+    // Update DOM
+    const titles = [dish, drink, dessert];
+    for (let i = 0; i < 3; i++) {
+        items[i].children[0].textContent = titles[i];
+        items[i].children[1].textContent = prices[i].toFixed(2).replace(".", ",");
+    }
+    items[3].children[1].textContent = "R$ " + total.replace(".", ",");
+    toggleConfirmScreen();
+}
+
 function executeOrder() {
-    const names = document.querySelectorAll("input:checked + label > div h4");
-    const prices = document.querySelectorAll("input:checked + label > div h5");
-
-    const [dish, drink, dessert] = [...names].map(e => e.textContent);
-    const total = [...prices].reduce((a, b) => a + priceToNumber(b.textContent), 0).toFixed(2);
-
+    const nome = prompt("Qual o seu nome?");
+    const endereco = prompt("Qual o seu endereço?");
     const url = "https://wa.me/5511996807608?text=" + encodeURIComponent(
-        `Olá, gostaria de fazer o pedido:\n- Prato: ${dish}\n- Bebida: ${drink}\n- Sobremesa: ${dessert}\nTotal: R$ ${total}`
+        `Olá, gostaria de fazer o pedido:\n- Prato: ${dish}\n- Bebida: ${drink}\n- Sobremesa: ${dessert}\nTotal: R$ ${total}\n\nNome: ${nome}\nEndereço: ${endereco}`
     );
-    openInNewTab(url);
+    window.open(url, '_blank').focus();
 }
 
 // Helpers
-function addInputListeners() {
-    inputs.forEach(e => e.addEventListener("click", validateForm));
-}
-
-function removeInputListeners() {
-    inputs.forEach(e => e.removeEventListener("click", validateForm));
-}
-
 function priceToNumber(str) {
-    return Number(str.replace("R$ ", "").replace(",", "."));
+    return +str.replace("R$ ", "").replace(",", ".");
 }
 
-function openInNewTab(url) {
-    window.open(url, '_blank').focus();
+function toggleConfirmScreen() {
+    document.querySelector(".confirm-wrapper").classList.toggle("hide");
 }
